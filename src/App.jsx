dd882bfb5eeb38d4a8c23a91cd1bf2a6ac531434
Ammar-Hus <velourvogue.lux@gmail.com>
2025-09-14 09:24:48 +0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className='min-h-screen bg-gradient-to-r from-indigo-100 via-white to-pink-50'>
+      <div className='p-4 text-center font-bold text-xl'>Gym Tracker App</div>
+      <div className='p-4'>[UI will render here in full version]</div>
+    </div>
+  );
+}
